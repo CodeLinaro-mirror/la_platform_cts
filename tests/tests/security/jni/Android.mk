1cda4b72ec1bd0343c371b8fee64d5dae04bc22c
Robert Shih <robertshih@google.com>
2015-09-03 15:49:14 -0700

    Test whether mediaserver leaks info across invalid binder calls
    Bug: 23756261
    Change-Id: I884fd26e723d119830cf70f1e1c78bdf46cded08
    (cherry picked from commit e943923cd60d85d6fecbea538628aecea4401bca)

--- tests/tests/security/jni/Android.mk
+++ tests/tests/security/jni/Android.mk
@@ -33,7 +33,8 @@ LOCAL_SRC_FILES := \
 		android_security_cts_AudioPolicyBinderTest.cpp \
 		android_security_cts_EncryptionTest.cpp \
 		android_security_cts_AudioFlingerBinderTest.cpp \
-		android_security_cts_MediaCryptoTest.cpp
+		android_security_cts_MediaCryptoTest.cpp \
+		android_security_cts_MediaPlayerInfoLeakTest.cpp
 
 LOCAL_C_INCLUDES := \
 	$(JNI_H_INCLUDE) \
