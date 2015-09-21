74c6d3e7d9a893aba10f2d98897b136b1e024cc2
Wei Jia <wjia@google.com>
2015-09-21 10:26:49 -0700

    rename android_security_cts_AudioflingerBinderTest.cpp to android_security_cts_AudioFlingerBinderTest.cpp
    Bug: 19573085.
    Bug: 20731946.
    Change-Id: I9705e3c3bbfc5d95af0708500885ad63b77e17bd

--- tests/tests/security/jni/Android.mk
+++ tests/tests/security/jni/Android.mk
@@ -30,7 +30,7 @@ LOCAL_SRC_FILES := \
 		android_security_cts_NativeCodeTest.cpp \
 		android_security_cts_LoadEffectLibraryTest.cpp \
 		android_security_cts_AudioPolicyBinderTest.cpp \
-		android_security_cts_AudioflingerBinderTest.cpp
+		android_security_cts_AudioFlingerBinderTest.cpp
 
 LOCAL_C_INCLUDES := $(JNI_H_INCLUDE)
 
